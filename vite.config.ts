a81766546278de26a976899b9174a4e28b2c851b
Duc Luong <duc.q.luong@gmail.com>
2025-03-30 23:38:55 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sleep-tracker-app",
+  base: "/sleep-tracker-app/",
   server: {
     port: 3000,
   },
